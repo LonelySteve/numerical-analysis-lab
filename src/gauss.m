bd5dfbe76184cd0a8d865843adc94f9228e357fd
jloeve <jianglufull@foxmail.com>
2020-06-06 13:25:20 +0800

    完成高斯消去法的程序

---
+++ src/gauss.m
@@ -0,0 +1,16 @@
+function [x] = gauss(A, b)
+    B = [A, b];
+    [n, m] = size(B);
+    % 求行阶梯形矩阵
+    for k = 1:n
+        for o = k+1:n
+            B(o, :) = B(o, :) - (B(o, k) / B(k, k)) * B(k,:);
+        end
+    end
+    disp(B)
+    x = zeros(m-1, 1);
+    % 回代求解
+    for k = n:-1:1
+       x(k) = (B(k, m)  - B(k, k+1: m-1) * x(k+1: m-1) ) / B(k, k);
+    end
+end
